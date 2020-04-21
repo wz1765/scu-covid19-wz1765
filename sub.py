134aa04e8eb3fc18187053da2083e8595a8b1b55
HyperMn <33088374+HyperMn@users.noreply.github.com>
2020-04-21 13:05:33 +0800

    Update sub.py

--- sub.py
+++ sub.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
 """
-author: Les1ie
-mail: me@les1ie.com
+Modified on Mon Apr 20 21:31:53 2020
+
+@author: Les1ie, HyperMn
+mail: me@les1ie.com, hypermn@163.com
 license: CC BY-NC-SA 3.0
 """
 
@@ -15,20 +18,20 @@ s = requests.Session()
 header = {"User-Agent": "Mozilla/5.0 (Linux; Android 10;  AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/6.2 TBS/045136 Mobile Safari/537.36 wxwork/3.0.16 MicroMessenger/7.0.1 NetType/WIFI Language/zh",}
 s.headers.update(header)
 
-user = "USERNAME"    # sep账号
-passwd = "PASSWORD"   # sep密码
+user = "USERNAME"    # 账号, 13位学号
+passwd = "PASSWORD"   # 密码， SCU统一认证登录密码
 api_key = "API_KEY"  # server酱的api，填了可以微信通知打卡结果，不填没影响
 
 
 def login(s: requests.Session, username, password):
-    # r = s.get(
-    #     "https://app.ucas.ac.cn/uc/wap/login?redirect=https%3A%2F%2Fapp.ucas.ac.cn%2Fsite%2FapplicationSquare%2Findex%3Fsid%3D2")
-    # print(r.text)
+    #r = s.get(
+    #    "https://app.ucas.ac.cn/uc/wap/login?redirect=https%3A%2F%2Fapp.ucas.ac.cn%2Fsite%2FapplicationSquare%2Findex%3Fsid%3D2")
+    #print(r.text)
     payload = {
         "username": username,
         "password": password
     }
-    r = s.post("https://app.ucas.ac.cn/uc/wap/login/check", data=payload)
+    r = s.post("https://wfw.scu.edu.cn/a_scu/api/sso/check", data=payload)
 
     # print(r.text)
     if r.json().get('m') != "操作成功":
@@ -38,8 +41,7 @@ def login(s: requests.Session, username, password):
 
 
 def get_daily(s: requests.Session):
-    daily = s.get("https://app.ucas.ac.cn/ncov/api/default/daily?xgh=0&app_id=ucas")
-    # info = s.get("https://app.ucas.ac.cn/ncov/api/default/index?xgh=0&app_id=ucas")
+    daily = s.get("https://wfw.scu.edu.cn/ncov/api/default/daily?xgh=0&app_id=scu")
     j = daily.json()
     d = j.get('d', None)
     if d:
@@ -52,30 +54,33 @@ def get_daily(s: requests.Session):
 
 def submit(s: requests.Session, old: dict):
     new_daily = {
-        'realname': old['realname'],
-        'number': old['number'],
-        'szgj_api_info': old['szgj_api_info'],
-        'sfzx': old['sfzx'],
-        'szdd': old['szdd'],
-        'ismoved': old['ismoved'],
-        'tw': old['tw'],
-        'sftjwh': old['sfsfbh'],
-        'sftjhb': old['sftjhb'],
-        'sfcxtz': old['sfcxtz'],
-        'sfjcwhry': old['sfjcwhry'],
-        'sfjchbry': old['sfjchbry'],
-        'sfjcbh': old['sfjcbh'],
-        'sfcyglq': old['sfcyglq'],
-        'sfcxzysx': old['sfcxzysx'],
-        'old_szdd': old['szdd'],
+        'realname': old['realname'],    #姓名
+        'number': old['number'],        #学工号
+        'sfzx': old['sfzx'],            #是否在校
+        'ismoved': old['ismoved'],      #?所在地点
+        'tw': old['tw'],                #体温
+        'sftjwh': old['sftjwh'],        #是否途经武汉
+        'sftjhb': old['sftjhb'],        #是否途经湖北
+        'sfcxtz': old['sfcxtz'],        #是否出现体征？
+        'sfjcwhry': old['sfjcwhry'],    #是否接触武汉人员
+        'sfjchbry': old['sfjchbry'],    #是否接触湖北人员
+        'sfjcbh': old['sfjcbh'],        #是否接触病患
+        'sfcyglq': old['sfcyglq'],      #是否处于隔离期
+        "sfjxhsjc": old['sfjxhsjc'],    #是否进行核酸检查
+        'sfcxzysx': old['sfcxzysx'],    #是否出现????
+        'szsqsfybl': old['szsqsfybl'],
+        'sfsqhzjkk': old['sfsqhzjkk'],
+        'sfygtjzzfj': old['sfygtjzzfj'],
+        'hsjcjg': old['hsjcjg'],
+        'old_szdd': old['old_szdd'],        #所在地点
+        'sfsfbh': old['sfsfbh'],            #是否??病患
         'geo_api_info': old['old_city'],
         'old_city': old['old_city'],
         'geo_api_infot': old['geo_api_infot'],
         'date': datetime.now(tz=pytz.timezone("Asia/Shanghai")).strftime("%Y-%m-%d"),
-        'jcjgqk': old['jcjgqk'],
-        'app_id': 'ucas'}
+        'app_id': 'scu'}
 
-    r = s.post("https://app.ucas.ac.cn/ncov/api/default/save", data=new_daily)
+    r = s.post("https://wfw.scu.edu.cn/ncov/api/default/save", data=new_daily)
     print("提交信息:", new_daily)
     # print(r.text)
     result = r.json()
@@ -99,6 +104,8 @@ def message(key, title, body):
 
 if __name__ == "__main__":
     print(datetime.now(tz=pytz.timezone("Asia/Shanghai")).strftime("%Y-%m-%d %H:%M:%S %Z"))
+    
+    #手动执行可注释以下三行的延迟填报
     for i in range(randint(10,600),0,-1):
         print("\r等待{}秒后填报".format(i),end='')
         sleep(1)
